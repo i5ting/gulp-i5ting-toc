ede44c156c6201705584cd004bc13a5bd8f51fc3
alfred sang <i5ting@126.com>
2014-11-15 13:19:48 +0800

    bump to v1.0.1

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var assign = require('object-assign');
-var fs = require('fs');
-var BufferHelper = require('bufferhelper');
-var Handlebars = require('handlebars');
-
 
 module.exports = function (options) {
-	
+
 	options = assign({}, options);
 
 	return through.obj(function (file, enc, cb) {
@@ -22,15 +18,6 @@ module.exports = function (options) {
 			return;
 		}
 		
-		var source_file = 'sample.md';
-
-		var markd_config = {
-			debug: false
-		}
-		
-	
-
 		require('i5ting_toc')(options.source_file, options.is_open, options.markd_config);
-		
 	});
 };
